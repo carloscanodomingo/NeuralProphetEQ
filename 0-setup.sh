b95caa23450a8e2c1e82fb42f1c6f20bcaf357a3
carloscano <carloscanodomingo@gmail.com>
2023-02-22 10:28:33 +0000

    Improved 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -2,5 +2,6 @@
 module load apps/binapps/anaconda3/2021.11
 module swap tools/env/proxy tools/env/proxy2
 conda create -n neuralprophet python=3.9.7
-conda activate neuralprophet
+conda activate neuralprophet  
 pip3 install -r requirements.txt
+EXPORT DATA_PATH="/mnt/hum01-home01/ambs/y06068cc/data"
